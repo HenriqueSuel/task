5985affac4c82548a56560502815a7733770e133
Henrique Suel <henriquesuel@TB000674.local>
2023-10-18 21:50:02 -0300

    aula 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { TaskListComponent } from './components/task-list/task-list.component';
 import { CurrencyFormatPipe } from './pipes/currency-format.pipe';
 import { TaskDetailComponent } from './components/task-detail/task-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { DateGreaterThanTodayDirective } from './directives/date-greater-than-today.directive';
+import { TaskFiltroComponent } from './components/task-filtro/task-filtro.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,6 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     TaskListComponent,
     CurrencyFormatPipe,
     TaskDetailComponent,
+    DateGreaterThanTodayDirective,
+    TaskFiltroComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit
+} from '@angular/core';
 import { Task } from 'src/models/task.model';
 
 @Component({
@@ -6,11 +13,18 @@ import { Task } from 'src/models/task.model';
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss'],
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
   isVisible = true;
 
   @Input() tasks: Task[] = [];
   @Output() handleTask = new EventEmitter();
+
+  tasksFiltradas: Task[] = [];
+
+
+  ngOnInit() {
+    this.tasksFiltradas = this.tasks;
+  }
 
   mostrarLista() {
     this.isVisible = !this.isVisible;
@@ -18,5 +32,19 @@ export class TaskListComponent {
 
   selectedTask(task: Task) {
     this.handleTask.emit(task);
+  }
+
+  handleFiltro(filtro: string) {
+    if (filtro === 'all') {
+      this.tasksFiltradas = this.tasks;
+      return;
+    }
+
+    this.tasksFiltradas = this.tasks.filter((item) => {
+      if (item.status === filtro) {
+        return item;
+      }
+      return;
+    });
   }
 }
